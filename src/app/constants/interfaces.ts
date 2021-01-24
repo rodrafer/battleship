bd8c0c3ffd3e2297861b76cab8e9ddeec42c14c5
Rodrigo Fernandez Frittelli <rodrigoff95@gmail.com>
2021-01-24 19:36:15 -0300

    Routing components with menu buttons

--- src/app/constants/interfaces.ts
+++ src/app/constants/interfaces.ts
@@ -1,22 +1,29 @@
 export interface SavedGameData {
-    name: string,
-    start_time: string,
-    turns_left: number,
-    accuracy: number
+    name: string;
+    start_time: string;
+    turns_left: number;
+    accuracy: number;
 }
 
 export interface PlayedGameData {
-    name: string,
-    start_time: string,
-    end_time: string,
-    turns_used: number,
-    accuracy: number,
-    status: string
+    name: string;
+    start_time: string;
+    end_time: string;
+    turns_used: number;
+    accuracy: number;
+    status: string;
 }
 
 export interface UserData {
-    uid: string,
-    saved_games: SavedGameData[],
-    played_games: PlayedGameData[],
-    multiplayer_code: string
+    uid: string;
+    saved_games: SavedGameData[];
+    played_games: PlayedGameData[];
+    multiplayer_code: string;
+}
+
+export interface MenuOptions {
+    name: string;
+    link: string;
+    anchor_id: string;
+    button_id: string;
 }
--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserData, MenuOptions } from '../constants/interfaces';
+import { FAKE_DATA, MENU_OPTIONS } from '../constants/constants';
 
 @Component({
   selector: 'app-main-menu',
@@ -7,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainMenuComponent implements OnInit {
   title = 'battleship';
+  user: UserData;
+  options: MenuOptions[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.user = FAKE_DATA[0];
+    this.options = MENU_OPTIONS;
   }
 
 }
