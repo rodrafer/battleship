01771a852f47822372105044abdfcf64e32e2134
Rodrigo Fernandez Frittelli <rodrigoff95@gmail.com>
2021-01-24 17:14:08 -0300

    Creating routs and setting up proyect to display the Main Menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { SavedGamesComponent } from './saved-games/saved-games.component';
+import { DifficultyComponent } from './difficulty/difficulty.component';
+import { PlayedGamesComponent } from './played-games/played-games.component';
+import { MultiplayerComponent } from './multiplayer/multiplayer.component';
+import { GameBoardComponent } from './game-board/game-board.component';
+import { FinalResultsComponent } from './final-results/final-results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'main-menu', pathMatch: 'full'},
+  {path: 'main-menu', component: MainMenuComponent, pathMatch: 'full'},
+  {path: 'saved-games/:id', component: SavedGamesComponent},
+  {path: 'difficulty', component: DifficultyComponent},
+  {path: 'played-games/:id', component: PlayedGamesComponent},
+  {path: 'multiplayer/:id', component: MultiplayerComponent},
+  {path: 'game-board/:id', component: GameBoardComponent},
+  {path: 'final-results/:id', component: FinalResultsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
