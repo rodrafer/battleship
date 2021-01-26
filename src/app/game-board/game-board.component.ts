4e109906f626695913b3c1aa8eba124c29d75f59
Rodrigo Fernandez Frittelli <rodrigoff95@gmail.com>
2021-01-25 22:20:33 -0300

    Creating table for the game board with cell names

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-board.component.scss']
 })
 export class GameBoardComponent implements OnInit {
+  boardTitle = 'battleship';
+  isLoading = false;
+  columns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
   constructor() { }
 
